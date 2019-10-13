17cda9f4d7b9144f692c26d18ee632a6b0f101b0
Eslam Saber <eslamsaber90@gmail.com>
2019-10-13 06:13:05 +0200

    intial commit

---
+++ adjustapi/dataset/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+class Dataset(models.Model):
+    date = models.DateTimeField()
+    channel = models.CharField(max_length=255)
+    country = models.CharField(max_length=50)
+    os = models.CharField(max_length=100)
+    impressions = models.IntegerField()
+    clicks = models.IntegerField()
+    installs = models.IntegerField()
+    spend = models.DecimalField(max_digits=9, decimal_places=3)
+    revenue = models.DecimalField(max_digits=9, decimal_places=3)
+    
+    def __str__(self):
+        return "{} - {}".format(self.channel, self.country)
+
+    def save(self, *args, **kwargs):
+        super(Dataset, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = ["-date"]
